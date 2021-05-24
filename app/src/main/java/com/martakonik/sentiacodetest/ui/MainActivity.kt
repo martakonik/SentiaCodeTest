d89eaf2a47e3abb1bfbd9f4f4207cdefc1254eac
martakonik <marta.maria.konik@gmail.com>
2021-05-24 16:57:23 +1000

    Add empty fragment and navigation

--- app/src/main/java/com/martakonik/sentiacodetest/ui/MainActivity.kt
+++ app/src/main/java/com/martakonik/sentiacodetest/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.martakonik.sentiacodetest
+package com.martakonik.sentiacodetest.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.martakonik.sentiacodetest.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
